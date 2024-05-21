ca99be750095e569fb88308f924b1004322d5600
Mathieu Ciancone <mathieu@wikit.ai>
2024-05-21 08:10:18 +0000

    Resolve "fix args unpacking"

--- src/custom_chunkers/wikit_chunknorris.py
+++ src/custom_chunkers/wikit_chunknorris.py
@@ -171,7 +171,7 @@ def main():
     args = parse_arguments()
 
     wcn = WikitChunkNorris()
-    wcn.chunk_directory(vars(args))
+    wcn.chunk_directory(**vars(args))
 
 
 if __name__ == "__main__":
