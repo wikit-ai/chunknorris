32aa863710acbfdb5d9b81c0c691d3f4bc5a4ade
Mathieu Ciancone <73994289+MathieuCiancone@users.noreply.github.com>
2025-03-27 11:01:58 +0100

    feat:improve md conversion (#27)
    Co-authored-by: Mathieu Ciancone <mathieu@wikit.ai>

--- src/chunknorris/parsers/sheets/csv_parser.py
+++ src/chunknorris/parsers/sheets/csv_parser.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 import pandas as pd
 
@@ -28,7 +29,7 @@ class CSVParser(AbstractParser):
             MarkdownDoc: the markdown-formatted csv.
         """
         df = self.read_file(filepath)
-        md_string = df.to_markdown(index=False)
+        md_string = CSVParser.convert_df_to_markdown(df)
 
         return MarkdownDoc.from_string(md_string)
 
@@ -85,3 +86,22 @@ class CSVParser(AbstractParser):
             raise ValueError(
                 "Could not detect the delimiter. You may want to set delimiter manually using SheetParser(csv_delimiter='<mydelimiter>') before parsing the file."
             ) from e
+
+    @staticmethod
+    def convert_df_to_markdown(df: pd.DataFrame) -> str:
+        """Converts a DataFrame to markdown.
+        Wraps tabula's method pd.DataFrame.to_markdown()
+        between pre and post processing.
+
+        Args:
+            df (pd.DataFrame): the dataframe to convert.
+
+        Returns:
+            str: a markdown formatted table.
+        """
+        df = df.apply(lambda x: x.str.replace("\n", " "))  # type: ignore | x: pd.Series -> pd.Series
+        md_string = df.to_markdown(index=False)
+        md_string = re.sub(r"\s{3,}", "  ", md_string)
+        md_string = re.sub(r"-{3,}", "---", md_string)
+
+        return md_string
