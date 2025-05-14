254a2bc9addb37b94329cd8d55c0e9e4a34d9625
Mathieu Ciancone <mathieu@wikit.ai>
2025-05-14 17:31:25 +0200

    update:pymupdf version

--- tests/chunknorris/chunkers/test_markdown_chunker.py
+++ tests/chunknorris/chunkers/test_markdown_chunker.py
@@ -14,12 +14,15 @@ def test_chunk_string(
         assert [chunk.get_text() for chunk in chunks] == output
 
 
-def test_split_big_chunks_wordbased(md_big_chunk_in: Chunk, md_big_chunk_out: list[str]):
+def test_split_big_chunks_wordbased(
+    md_big_chunk_in: Chunk, md_big_chunk_out: list[str]
+):
     md_chunker = MarkdownChunker(
         max_chunk_word_count=0, hard_max_chunk_word_count=10, min_chunk_word_count=0
     )
     assert [
-        chunk.get_text() for chunk in md_chunker.split_big_chunks_wordbased([md_big_chunk_in])
+        chunk.get_text()
+        for chunk in md_chunker.split_big_chunks_wordbased([md_big_chunk_in])
     ] == md_big_chunk_out
 
 
